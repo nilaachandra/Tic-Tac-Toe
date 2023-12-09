944c62987462627c842d358332d7ab3287fe25cf
Nila Chandra Laishram <nilalaishram@gmail.com>
2023-12-09 09:10:34 +0530

    innitial commit

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnO) {
       //playerO
-      box.innerText = "O";
+      box.innerText = "X";
       turnO = false;
     } else {
       //playerX
-      box.innerText = "X";
+      box.innerText = "O";
       turnO = true;
     }
     box.disabled = true;
